5f41c6c26202a165a89c3f89b286d64636e70a62
Amjad Khazem <akhazem2007@gmail.com>
2026-03-25 18:02:06 -0500

    Test CI/CD pipeline

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Server is running!')
+  res.send({ message: 'Socratic API is live 🚀' })
 })
 
 // -----------------------------------------------------
