fce17e32fa9503832fb50309bee22c0b72ea77f7
Snigdhahy3-dev <snigdhahy3@gmail.com>
2026-04-20 18:43:11 -0500

    remove .next from tracking

--- .next/dev/static/chunks/app_page_07a761c8.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_components_GoogleSignInButton_3d13186a.js"
-],
-    source: "dynamic"
-});
